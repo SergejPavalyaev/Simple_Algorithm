5bdcf4470c8c9b358c99e8cce52884fd091e305d
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-26 20:03:34 +0300

    Taks 1

---
+++ ДЗ_Урок2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ДЗ_Урок2
+{
+    public class Program{
+        public static void Main (string [] args){
+            //Task_1
+            System.Console.Write("Task 1. Input number => ");
+            int number = InputNumber();
+            System.Console.WriteLine(number);
+        }
+        public static int InputNumber() {
+            string ? numString = System.Console.ReadLine();
+            int numInt;
+            while (! Int32.TryParse(numString, out numInt))
+            {
+                System.Console.WriteLine("Error, try again");
+                numString = Console.ReadLine();
+            }
+            return numInt;
+        }
+
+    }
+}
