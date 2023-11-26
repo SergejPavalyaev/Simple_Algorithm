fb43bb88476296e092fd97b031f9640a4998b21d
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-26 23:39:50 +0300

    Task 2

--- ДЗ_Урок3/Program.cs
+++ ДЗ_Урок3/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ДЗ_Урок2{
+    public class Program{
+        public static void Main (string [] args){
+            System.Console.WriteLine("Task 1");
+            int limitDown = 1;
+            int limitUP = 100;
+            int size = 10;
+            int [] array = InitArray (limitDown, limitUP, size);
+            PrintArray(array);
+            int searchDown = 20;
+            int searchUp = 90;
+            int count = CountArrayInLimit (array, searchDown, searchUp);
+            System.Console.WriteLine($"{count} elements in [{searchDown},{searchUp}]");
+            System.Console.WriteLine("Task 2");
+            count = CountEvenNumber(array);
+            System.Console.WriteLine($"{count} elemets is even");
+        }
+        public static int CountEvenNumber (int [] array){
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item % 2 == 0) count++;
+            }
+            return count;
+        }
+        public static int [] InitArray (int limitDown, int limitUP, int size){
+            Random rand = new Random();
+            int [] array = new int [size];
+            int temp;
+            if (limitDown > limitUP) {
+                temp = limitDown;
+                limitDown = limitUP;
+                limitUP = temp;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(limitDown, limitUP);
+            }
+            return array;
+        }
+        public static void PrintArray (int [] array){
+            foreach (int item in array)
+            {
+                System.Console.Write(item + " ");
+            }
+            System.Console.WriteLine();            
+        }
+        public static int CountArrayInLimit (int [] array, int searchDown, int searchUp){
+            int count = 0;
+            int temp;
+            if (searchDown > searchUp) {
+                temp = searchDown;
+                searchDown = searchUp;
+                searchUp = temp;
+            }
+            foreach (int item in array)
+            {
+                if (item > searchDown & item < searchUp) count++;
+            }
+            return count;
+        }
+    }
+}
