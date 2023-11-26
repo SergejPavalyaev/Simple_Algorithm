b136bcc18e3a55008d8ee1b1f30e6519f4ffa65c
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-26 22:02:37 +0300

    Taks 3

--- ДЗ_Урок2/Program.cs
+++ ДЗ_Урок2/Program.cs
@@ -2,28 +2,28 @@
 {
     public class Program{
         public static void Main (string [] args){
-            //Task_1
-            System.Console.Write("Task 1. Input number => ");
-            int number = InputNumber();
-            if (number % 7 == 0 & number % 23 == 0)
-            {
-                System.Console.WriteLine($"Number {number} is divided on 7 and 23.");
-            } else
-            {
-                System.Console.WriteLine($"Number {number} isn't divided on 7 and 23.");
+            int number;
+            // System.Console.Write("Task 1. Input number => ");
+            // number = InputNumber();
+            // if (number % 7 == 0 & number % 23 == 0)
+            // {
+            //     System.Console.WriteLine($"Number {number} is divided on 7 and 23.");
+            // } else
+            // {
+            //     System.Console.WriteLine($"Number {number} isn't divided on 7 and 23.");
 
-            }
-            System.Console.WriteLine("Task 2");
-            int limit = 0;
-            System.Console.WriteLine("Input X => ");
-            int X = InputNumberNoNull(limit);
-            System.Console.WriteLine("Input X => ");
-            int Y = InputNumberNoNull(limit);
-            System.Console.WriteLine(CoordinateQuarter(X,Y));
+            // }
+            // System.Console.WriteLine("Task 2");
+            // int limit = 0;
+            // System.Console.Write("Input X => ");
+            // int X = InputNumberNoNull(limit);
+            // System.Console.Write("Input Y => ");
+            // int Y = InputNumberNoNull(limit);
+            // System.Console.WriteLine(CoordinateQuarter(X,Y));
             System.Console.WriteLine("Task 3");
-            int limit1 = 10;
-            int limit2 = 99;
-            int number = InputNumberInLimit(limit1, limit2);
+            System.Console.Write("Input number in 10 at 99 => ");
+            number = InputNumberInLimit();
+            System.Console.WriteLine($"Max numeric is {MaxNumeric(number)}");
         }
         public static int InputNumber() {
             string ? numString = System.Console.ReadLine();
@@ -38,11 +38,7 @@
         public static int InputNumberNoNull(int limit) {
             string ? numString = System.Console.ReadLine();
             int numInt;
-            while (! Int32.TryParse(numString, out numInt))
-            {
-                System.Console.WriteLine("Error, try again");
-                numString = Console.ReadLine();
-            }
+            Int32.TryParse(numString, out numInt);
             while (numInt == limit)
             {
                 System.Console.WriteLine("Input no null, try again");
@@ -51,19 +47,19 @@
             }
             return numInt;
         }
-        public static int InputNumberInLimit(int limit1, int limit2) {
+        public static int InputNumberInLimit() {
             string ? numString = System.Console.ReadLine();
             int numInt;
-            while (! Int32.TryParse(numString, out numInt))
+            Int32.TryParse(numString, out numInt);
+            bool flag;
+            if (Math.Log10(numInt) >= 1 & Math.Log10(numInt) < 2) flag = false;
+            else flag = true;
+            while (flag)
             {
-                System.Console.WriteLine("Error, try again");
-                numString = Console.ReadLine();
-            }
-            while (numInt < limit1 & numInt > limit2)
-            {
-                System.Console.WriteLine("Input no null, try again");
+                System.Console.WriteLine("Input isn't at [10,99], try again");
                 numString = Console.ReadLine();
                 Int32.TryParse(numString, out numInt);
+                if (Math.Log10(numInt) >= 1 & Math.Log10(numInt) < 2) flag = false;
             }
             return numInt;
         }
@@ -71,7 +67,13 @@
             if (X > 0 & Y > 0) return "Dot is the I Quarter";
             if (X > 0 & Y < 0) return "Dot is the II Quarter";
             if (X < 0 & Y > 0) return "Dot is the III Quarter";
-            return "Dot is the VI Quarter";
+            return "Dot is the IV Quarter";
+        }
+        public static int MaxNumeric (int number){
+            int temp1 = number / 10;
+            int temp2 = number % 10;
+            if (temp1 >= temp2) return temp1;
+            else return temp2;
         }
     }
 }
