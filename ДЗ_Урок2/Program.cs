1813efb347f5c4aa84b6935077f8a14596cdeeb0
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-26 20:13:43 +0300

    Taks 1.1

--- ДЗ_Урок2/Program.cs
+++ ДЗ_Урок2/Program.cs
@@ -5,7 +5,15 @@
             //Task_1
             System.Console.Write("Task 1. Input number => ");
             int number = InputNumber();
-            System.Console.WriteLine(number);
+            if (number % 7 == 0 & number % 23 == 0)
+            {
+                System.Console.WriteLine($"Number {number} is divided on 7 and 23.");
+            } else
+            {
+                System.Console.WriteLine($"Number {number} isn't divided on 7 and 23.");
+
+            }
+            // System.Console.WriteLine(number);
         }
         public static int InputNumber() {
             string ? numString = System.Console.ReadLine();
