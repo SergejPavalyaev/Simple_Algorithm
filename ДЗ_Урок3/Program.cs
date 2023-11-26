7edc577854400cd738e314b8323b9b0f9e3476ac
Sergey Pavalyaev <serg-paval@inbox.ru>
2023-11-27 00:13:54 +0300

    Task 3

--- ДЗ_Урок3/Program.cs
+++ ДЗ_Урок3/Program.cs
@@ -14,6 +14,22 @@
             System.Console.WriteLine("Task 2");
             count = CountEvenNumber(array);
             System.Console.WriteLine($"{count} elemets is even");
+            System.Console.WriteLine("Task 3");
+            double [] doubleArray = InitDoubleArray (limitDown, limitUP, size);
+            PrintDoubleArray(doubleArray);
+            System.Console.WriteLine($"Substraction Max and Min = {SubtMinMax(doubleArray)}");
+        }
+        public static double SubtMinMax (double [] array){
+            double max = array [0];
+            double min = array [0];
+            double result;
+            foreach (double item in array)
+            {
+                if (item < min) min = item;
+                if (item > max) max = item;
+            }
+            result = max - min;
+            return result;
         }
         public static int CountEvenNumber (int [] array){
             int count = 0;
@@ -38,8 +54,30 @@
             }
             return array;
         }
+        public static double [] InitDoubleArray (int limitDown, int limitUP, int size){
+            Random rand = new Random();
+            double [] array = new double [size];
+            int temp;
+            if (limitDown > limitUP) {
+                temp = limitDown;
+                limitDown = limitUP;
+                limitUP = temp;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rand.Next(limitDown, limitUP) + rand.NextDouble();
+            }
+            return array;
+        }
         public static void PrintArray (int [] array){
             foreach (int item in array)
+            {
+                System.Console.Write(item + " ");
+            }
+            System.Console.WriteLine();            
+        }
+        public static void PrintDoubleArray (double [] array){
+            foreach (double item in array)
             {
                 System.Console.Write(item + " ");
             }
